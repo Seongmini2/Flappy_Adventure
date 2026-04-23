6f099dc9c3dadb3a8d65170761563b759f431c16
wnsgur070815-debug <leejunhyeok7777@gmail.com>
2026-04-24 02:45:50 +0900

    [Update] 클리어 처리
    -스테이지 1 마지막 파이프 넘어가면 클리어 화면
    -클리어 화면에서 스테이지로 넘어가는 버튼

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
-
+using System.Collections;
 public class Player : MonoBehaviour
 {
     public GameObject GameOverUI;
+
+    public GameObject GameFinish;   
     private Rigidbody2D rb;
     public float jumpForce = 5f; // jumpForce = 점프 세기를 조절하기 위한 이름
-    private bool isDead; // isDead = 이미 죽었는지 막기 위한 이름
+    public bool isDead; // isDead = 이미 죽었는지 막기 위한 이름
 
     public GameObject BulletPrefab;
     public float BulletSpeed = 30f;
@@ -23,10 +25,38 @@ public class Player : MonoBehaviour
         BulletControl();
     }
 
+    
+    
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        GameOver();
+        if (collision.gameObject.CompareTag("Finishpipe"))
+        {
+            GameClear(collision);
+        }
+        else { GameOver(); }
     }
+
+    // 2. 그냥 통과했을 때 (isTrigger 켜짐)
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Finishpipe"))
+        {
+            // 굳이 GameClear(collision) 안 거치고 바로 실행해도 됩니다.
+            StartCoroutine(DelayTime());
+        }
+    }
+    
+    void GameClear(Collision2D other)
+    {
+        StartCoroutine(DelayTime());
+    }
+
+    IEnumerator DelayTime() //UI를 1초후 키기 위한 코딩
+        {
+            yield return new WaitForSeconds(1.0f);
+            GameFinish.SetActive(true);
+        }
 
     void GameOver()
     {
